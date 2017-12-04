cf2ea66be0b53f9e90a3d07542735e961ea7e59d
GremSnoort <grem_snoort@protonmail.com>
2017-12-04 22:05:21 +0000

    style for second form (that is for creation new file)

--- optionsfornewfile.cpp
+++ optionsfornewfile.cpp
@@ -6,6 +6,19 @@ OptionsForNewFile::OptionsForNewFile(QWidget *parent) :
     ui(new Ui::OptionsForNewFile)
 {
     ui->setupUi(this);
+    QFont allFont("Misc Fixed", 16, QFont::Bold);
+
+    QString style = "color: rgb(160, 200, 180);\n";
+    QString style1 = "background-color: rgb(35, 38, 49);color: rgb(160, 200, 180);\n";
+
+    ui->Cancel->setStyleSheet(style1);
+    ui->OK->setStyleSheet(style1);
+    ui->Cancel->setFont(allFont);
+    ui->OK->setFont(allFont);
+    ui->label->setFont(allFont);
+    ui->label->setStyleSheet(style);
+    ui->label_2->setFont(allFont);
+    ui->label_2->setStyleSheet(style);
 
 
     connect(ui->OK, SIGNAL(released()), this, SLOT(pressedOK()));
